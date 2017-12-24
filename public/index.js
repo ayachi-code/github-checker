ba0006dd1254741c6797eaf1b65f2b074ed7d88b
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-24 12:43:02 +0100

    gaat naar een bestand

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 var verzenden_knop = document.getElementById('verzenden').addEventListener('click',() => {
     var gebruiker = document.getElementById('zoeka').value;
     localStorage.setItem("gebruikersnaam", gebruiker);
+    window.open("gegevens/index.html");
 });
