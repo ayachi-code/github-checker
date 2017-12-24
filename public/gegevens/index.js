860800853258d7f9a3f31b8ba185a0dff9aeba88
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-24 14:59:02 +0100

    Authenticatie bug gefixt

--- public/gegevens/index.js
+++ public/gegevens/index.js
@@ -2,13 +2,16 @@
 var gh = new GitHub({
    username: 'marker123',
    password: 'iphone5s',
-   token: '8b205b9610f534e285d29a2f737d2927076b131c'
+   token: 'f32d39543537f723d39cbe3b8ea45f5ba0e0579b'
 
 });
 
 var naam = localStorage.getItem("gebruikersnaam");
 
 
-var gebruiker = gh.getUser("ayachi");
+var gebruiker = gh.getUser("ayachi-code");
 
-gebruiker.follow();
+
+gebruiker.getProfile(function (err,cb) {
+      console.log(cb);
+});
