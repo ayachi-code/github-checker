c0cdb932214c264f33fb6d64f2b99448cd66e3cf
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-24 12:34:27 +0100

    Value van input gepakt

--- public/index.js
+++ public/index.js
@@ -1,3 +1,6 @@
-var verzenden_knop = document.getElementById('verzenden').addEventListener('click',() => {
+var gebruiker = document.getElementById('zoeka').value;
 
+
+var verzenden_knop = document.getElementById('verzenden').addEventListener('click',() => {
+    console.log("hey");
 });
